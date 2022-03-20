1a827fbca42a01fab191724e272e3317baae2349
Metodi Angelov <101995681+Ubersicka@users.noreply.github.com>
2022-03-20 23:34:54 +0200

    Create numbers_from_1_to_100.py

---
+++ numbers_from_1_to_100.py
@@ -0,0 +1,14 @@
+# for i in range (1, 101):     Повторения с цикъл
+#     print(i)
+
+# for i in range(2 , 11, 2):     цикъл със стъпка
+#     print(i)
+
+# for i in range(10, -1, -1):     цикъл със намаляване
+#      print(i)
+
+# num = 10                       пример
+# start = 100
+# end = 200
+# for i in range(start, end + 1, num):
+#     print(i, end = "")
